3907917ac3ff74db3a222315d63bf9937a177399
Ilya <ilya-katkov-95@mail.ru>
2023-08-07 17:18:16 +0300

    add new_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def capital_letters(line):
+    """Функция, которая возвращает строку с заглавными буквами"""
     return line.upper()
+
+def title(line):
+    """Делает заглавными первые буквы каждого слова"""
+    return line.title()
